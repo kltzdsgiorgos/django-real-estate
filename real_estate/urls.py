fef579bf2ab8b2ccd90890fc81465493244a82bb
Georgios Kalaitzidis <giorgoskltzds@gmail.com>
2022-07-26 01:50:20 +0300

    add static, images, requirements

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
 
 from listings.views import (
@@ -8,6 +10,7 @@ from listings.views import (
     listing_update,
     listing_delete
 )
+from real_estate.settings import MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,3 +20,6 @@ urlpatterns = [
     path('listings/<pk>/delete/', listing_delete),
     path('add-listing/', listing_create),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
